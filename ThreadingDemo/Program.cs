0b83ae7bb58fc1c9245f4976af65a98ace4c8b2c
RohitLonkar3114 <rohitlonkar3114@gmail.com>
2025-07-28 08:12:40 +0530

    Add Errors in Threading Demo Project

--- ThreadingDemo/Program.cs
+++ ThreadingDemo/Program.cs
@@ -15,8 +15,8 @@ namespace ThreadingDemo
             Monitor.Enter(this);
                 for(int i=1;i<=5;i++)
                 {
-                    Console.WriteLine(s);
-                    Thread.Sleep(1000);
+                    Console.WriteLine(s)
+                    Thread.Sle(1000);
                 }
             Monitor.Exit(this);
             //}
